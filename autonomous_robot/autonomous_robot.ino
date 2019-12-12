965c89f7bc95412330023b2763d539f57f827b06
nicordev <nicolas.renvoise.dev@gmail.com>
2019-12-12 23:01:42 +0100

    Move backward

--- autonomous_robot/autonomous_robot.ino
+++ autonomous_robot/autonomous_robot.ino
@@ -16,13 +16,6 @@ int rightMotorPins[3] = {2, 3, 4};
 // Left motor (L293D channels 1 and 2)
 int leftMotorPins[3] = {8, 9, 10};
 
-// const int RIGHT_MOTOR_PIN_ACTIVATE_3_4 = 2;
-// const int RIGHT_MOTOR_PIN_INPUT_3 = 3;
-// const int RIGHT_MOTOR_PIN_INPUT_4 = 4;
-// const int LEFT_MOTOR_PIN_ACTIVATE_1_2 = 8;
-// const int LEFT_MOTOR_PIN_INPUT_1 = 9;
-// const int LEFT_MOTOR_PIN_INPUT_2 = 10;
-
 /**
  * Initialize the pins of a motor
  */
@@ -35,6 +28,9 @@ void initMotor(int activationPin, int inputPin1, int inputPin2)
     digitalWrite(activationPin, HIGH);
 }
 
+/**
+ * Move the robot forward
+ */
 void moveForward(int *leftMotorPins, int *rightMotorPins)
 {
     digitalWrite(leftMotorPins[INPUT_PIN_1], HIGH);
@@ -43,31 +39,15 @@ void moveForward(int *leftMotorPins, int *rightMotorPins)
     digitalWrite(rightMotorPins[INPUT_PIN_2], HIGH);
 }
 
+/**
+ * Move the robot backward
+ */
 void moveBackward(int *leftMotorPins, int *rightMotorPins)
 {
     digitalWrite(leftMotorPins[INPUT_PIN_1], LOW);
     digitalWrite(leftMotorPins[INPUT_PIN_2], HIGH);
     digitalWrite(rightMotorPins[INPUT_PIN_1], HIGH);
     digitalWrite(rightMotorPins[INPUT_PIN_2], LOW);
-}
-
-/**
- * Run a motor either clockwise or counter-clockwise
- */
-void runMotor(int pin1, int pin2, bool clockwise = true)
-{
-    if (clockwise)
-    {
-        // Run the motor clockwise
-        digitalWrite(pin1, HIGH);
-        digitalWrite(pin2, LOW);
-    }
-    else
-    {
-        // Run the motor counter-clockwise
-        digitalWrite(pin1, LOW);
-        digitalWrite(pin2, HIGH);
-    }
 }
 
 // Main functions
@@ -85,16 +65,13 @@ void loop()
     Serial.print(distance);
     Serial.println("cm");
 
-    moveForward(leftMotorPins, rightMotorPins);
+    if (distance > 50) {
+        moveForward(leftMotorPins, rightMotorPins);
+    }
+    
+    if (distance < 20) {
+        moveBackward(leftMotorPins, rightMotorPins);
+    }
+
     delay(1000);
-
-    // Clockwise
-    // runMotor(PIN_INPUT_1, PIN_INPUT_2, true);
-    // runMotor(PIN_INPUT_3, PIN_INPUT_4, true);
-    // delay(2000);
-
-    // Counter-clockwise
-    // runMotor(PIN_INPUT_1, PIN_INPUT_2, false);
-    // runMotor(PIN_INPUT_3, PIN_INPUT_4, false);
-    // delay(2000);
 }
